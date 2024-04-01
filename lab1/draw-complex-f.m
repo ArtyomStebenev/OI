c3b1c8ae43b591e939cbd9c798a4d8208fe09264
ArtyomStebenev <artyomchiken@gmail.com>
2024-04-01 22:23:45 +0400

    improve legend

--- lab1/draw-complex-f.m
+++ lab1/draw-complex-f.m
@@ -1,11 +1,14 @@
 plot(x(1:10:end), real(f)(1:10:end), "-g; Re(f(x));");
+legend("Location", "northeastoutside");
 
 figure;
 plot(x(1:10:end), imag(f)(1:10:end), "-m; Im(f(x));");
+legend("Location", "northeastoutside");
 
 figure;
 plot(x(1:10:end), abs(f)(1:10:end), "-.r; Abs(f(x));");
+legend("Location", "northeastoutside");
 
 figure;
 plot(x(1:10:end), arg(f)(1:10:end), ".b; Arg(f(x));");
-
+legend("Location", "northeastoutside");
--- lab1/draw-h.m
+++ lab1/draw-h.m
@@ -2,12 +2,14 @@ h = f .* exp(i .* x);
 
 plot(x(1:5:end), real(h(1:5:end)), "-g; Re(h);",
      x(1:5:end), arg(h)(1:5:end), ".b; Arg(h);");
+legend("Location", "northeastoutside");
 title("h(x) = f(x)*exp(ix)");
 
 figure;
-plot(x(1:5:end), imag(h(1:5:end)), "-m; Im(h);",
+plot(x(1:5:end), imag(h(1:5:end)), "-m; Im(h);");
+legend("Location", "northeastoutside");
 
 figure;
-plot(x(1:5:end), abs(h)(1:5:end), "-.r; Abs(h);",
-
+plot(x(1:5:end), abs(h)(1:5:end), "-.r; Abs(h);");
+legend("Location", "northeastoutside");
 
