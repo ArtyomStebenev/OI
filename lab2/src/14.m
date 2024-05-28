5f7bfb4e1488a7629e974cdb8b194f6fd8fce003
ArtyomStebenev <artyomchiken@gmail.com>
2024-05-28 15:02:02 +0400

    add report to lab2

--- lab2/src/14.m
+++ lab2/src/14.m
@@ -1,7 +1,7 @@
 #14
 
 plot(x(1:10:end), (f)(1:10:end), ".r",
-     x(1:10:end), (Revers_F)(1:10:end), "-.y");
+     x(1:10:end), (Revers_f)(1:10:end), "-.y");
 title("f(x) and reverse f(x)");
 legend('f(x)', 'reverse(f)', 'Location', 'bestoutside')
 
--- lab2/src/analiticSolving.m
+++ lab2/src/analiticSolving.m
@@ -1,10 +1,13 @@
 analitic_F = 2 * sinc(2*z).^2;
 
-plot(z(1:4:end), abs(F)(1:4:end), ".r; abs(F(z));",
-     z(1:4:end), abs(analitic_F(1:4:end)), "-y; analitic abs(F(z));");
+plot(z(1:2:end), abs(F)(1:2:end), ".r",
+     z(1:4:end), abs(analitic_F(1:4:end)), "-y");
+title("digital and analitic Abs(F(z))");
+legend('digital', 'analitic', 'Location', 'bestoutside');
 
 
 figure;
-plot(z(1:2:end), arg(F)(1:2:end), ".b; arg(F(z));",
-     z(1:2:end), arg(analitic_F(1:2:end)), "-g; analitic arg(F(z));");
-
+plot(z(1:10:end), arg(F)(1:10:end), ".b",
+     z(1:2:end), arg(analitic_F(1:2:end)), "-g");
+title("digital and analitic Arg(F(z))");
+legend('digital', 'analitic', 'Location', 'bestoutside');
